9f74320a7c84da4740b7b3ddf786607c7f8ed65b
Pierre Gueroult <pierregueroult2022@gmail.com>
2025-11-13 16:45:36 +0100

    build(portfolio): add Docker support for apps/portfolio

--- apps/portfolio/next.config.ts
+++ apps/portfolio/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   transpilePackages: ['@repo/ui'],
+  output: 'standalone',
 };
 
 const withNextIntl = createNextIntlPlugin({
